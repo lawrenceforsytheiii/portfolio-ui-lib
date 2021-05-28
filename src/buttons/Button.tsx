de0680717288764460b4982e8127855d9c7b1aed
lawrenceforsytheiii <lawrence@whispir.com>
2021-05-28 14:11:34 -0700

    basic button api setup

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export const Button: React.FC = () => {
-  return <button>Hello world</button>;
-};
+export const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<'button'>
+>(({ children, ...props }, ref) => {
+  return <button ref={ref} type="button" {...props}>{children}</button>;
+});
+
+Button.displayName = 'Button';
